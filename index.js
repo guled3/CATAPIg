5866e8e0348bbfa6ac3c04bd58f1553c920de9fa
guled3 <56385581+guled3@users.noreply.github.com>
2019-12-25 19:14:16 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './CATAPI_basics';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+    <div>
+        <CATAPI_basics/>
+    </div>
+)
+
