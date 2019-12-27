3d6672d1ef04218442b174224a2417df4f5e9dd8
Cody Jones <magerx1794@gmail.com>
2019-12-26 23:12:49 -0600

    Updating to hooks

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { getBreeds, getCatsImagesByBreed} from '../src/CatApi';
 import './App.css';
 
